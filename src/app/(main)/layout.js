e2492cb19e87628f0c819a5ae929a0284d8f496a
nazmulapi <nazmulhussain.api@gmail.com>
2024-01-01 11:38:45 +0600

    Update to server side authentication

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,10 +1,10 @@
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import { authTokenKey } from "../../lib/config";
-import Accessible from "../../components/utils/CheckAccessibleForMainPages";
+// import { authTokenKey } from "../../lib/config";
+// import Accessible from "../../components/utils/CheckAccessibleForMainPages";
 import SideMenu from "../../components/app-layouts/SideMenu";
 import Navbar from "../../components/app-layouts/TopNavbar";
-
+import { checkIsAuthenticated } from "../../lib/check";
 import "../../styles/main.scss";
 
 export const metadata = {
@@ -13,15 +13,21 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
-  const cookieStore = cookies();
-  let token = cookieStore.get(authTokenKey);
-  if (token === undefined || token === null) {
+  // const cookieStore = cookies();
+  // let token = cookieStore.get(authTokenKey);
+  // if (token === undefined || token === null) {
+  //   redirect("/auth/login");
+  // }
+
+  const isAuthenticated = await checkIsAuthenticated();
+
+  if (!isAuthenticated) {
     redirect("/auth/login");
   }
 
   return (
     <>
-      <Accessible />
+      {/* <Accessible /> */}
       <div className="wrapper">
         <div className="side_nav">
           <SideMenu />
